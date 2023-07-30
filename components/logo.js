204fe18e8f8685c9fd0b34cf95ccdc0d532ec0b5
septan4 <sepehr75t@gmail.com>
2023-07-30 15:48:11 +0200

    commit

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,5 @@
-// import Link from 'next/link'
-import { Text, useColorModeValue } from '@chakra-ui/react'
-// import FootprintIcon from './icons/footprint'
+import { Text, useColorModeValue, Box } from '@chakra-ui/react'
+import FootprintIcon from './icons/footprint'
 import styled from '@emotion/styled'
 import { Link as ScrollLink } from 'react-scroll'
 
@@ -12,7 +11,6 @@ const LogoBox = styled.span`
   height: 30px;
   line-height: 20px;
   padding: 10px;
-
   > svg {
     transition: 200ms ease;
   }
@@ -32,18 +30,21 @@ const Logo = () => {
       offset={-70}
       duration={1000}
     >
-      <LogoBox>
-        {/* <FootprintIcon /> */}
-        <Text
-          cursor="pointer"
-          color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-          fontFamily='M PLUS Rounded 1c", sans-serif'
-          fontWeight="bold"
-          ml={3}
-        >
-          Sourena Lalehzari
-        </Text>
-      </LogoBox>
+      <Box display="flex" flexDirection="column" height="100%">
+        <LogoBox>
+          <FootprintIcon />
+          <Text
+            cursor="pointer"
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily='M PLUS Rounded 1c", sans-serif'
+            fontWeight="bold"
+            ml={3}
+          >
+            Sourena Lalehzari
+          </Text>
+        </LogoBox>
+        <Box mt="auto" />
+      </Box>
     </ScrollLink>
   )
 }
